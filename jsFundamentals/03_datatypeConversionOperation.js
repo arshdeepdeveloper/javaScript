80b7e75ad82ad228370d311a123b9553c863296f
arshdeepdeveloper <arshdeepsingh.gill8@gmail.com>
2024-09-06 09:22:04 +1200

    dataTypeConvertion

---
+++ jsFundamentals/03_datatypeConversionOperation.js
@@ -0,0 +1,12 @@
+let score = 1;
+
+console.log(typeof score); // number
+console.log(typeof (score)); // number
+
+let valueinNumber = Array(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber)
+
+// String 
+// boolean
+
