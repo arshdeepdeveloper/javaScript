b24df943d86ac3716510c08fd0fc7b98fc2a67d0
arshdeepdeveloper <arshdeepsingh.gill8@gmail.com>
2024-09-06 09:03:16 +1200

    dataTypes

---
+++ jsFundamentals/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as newer Version
+
+// number 
+// String
+// boolean
+// null
+// undefined // 
+// Symbol // for uniqueness
+
+// object 
+
+
+let state = null; // standalone value
+
+console.log(typeof null) // output object
+console.log(typeof undefined) // 
+
